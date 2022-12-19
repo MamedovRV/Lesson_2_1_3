62c6bad4d4e3f2adada4e9aff3540a2a23401767
MamedovRV <120065825+MamedovRV@users.noreply.github.com>
2022-12-19 19:55:43 +0700

    Lesson_2_1_3
    First commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,23 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+                applicationContext.getBean("helloworld", HelloWorld.class);
         System.out.println(bean.getMessage());
+        HelloWorld beanTwo =
+                applicationContext.getBean("helloworld", HelloWorld.class);
+
+        Cat one =applicationContext.getBean("cat", Cat.class);
+        Cat two =applicationContext.getBean("cat", Cat.class);
+
+        System.out.println("Hello world: " + (bean == beanTwo));
+        System.out.println("Cat: " + (one == two));
+
+
     }
 }
